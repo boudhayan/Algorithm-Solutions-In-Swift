03cb304bfac6528c8325884dcaccdce3f0948fcf
Boudhayan Biswas <hello@boudhayan.com>
2021-07-18 02:11:26 +0530

    Add test target

---
+++ Algorithm Solutions In Swift Tests/Algorithm_Solutions_In_Swift_Tests.swift
@@ -0,0 +1,32 @@
+//
+//  Algorithm_Solutions_In_Swift_Tests.swift
+//  Algorithm Solutions In Swift Tests
+//
+//  Created by Boudhayan Biswas on 18/07/21.
+//
+
+import XCTest
+
+class Algorithm_Solutions_In_Swift_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
