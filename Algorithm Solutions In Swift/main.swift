ce9954bcf28633377cffe5bca91fb620cfb7e31e
Boudhayan Biswas <hello@boudhayan.com>
2021-07-18 10:03:36 +0530

    Add move element to end

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 print("Hello, World!")
-var arrayOne = [-1, 5, 10, 20, 28, 3]
+var arrayOne = [2, 1, 2, 2, 2, 3, 4, 2]
 var arrayTwo = [26, 134, 135, 15, 17]
-print(smallestDifference(arrayOne: &arrayOne, arrayTwo: &arrayTwo))
+print(moveElementToEnd(array: &arrayOne, 2))
