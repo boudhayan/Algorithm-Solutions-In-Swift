cb614267c4a9e8be839b3ab59cb2350c9ccb996c
Boudhayan Biswas <hello@boudhayan.com>
2021-07-18 10:32:10 +0530

    Add monotonic array

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -10,4 +10,4 @@ import Foundation
 print("Hello, World!")
 var arrayOne = [2, 1, 2, 2, 2, 3, 4, 2]
 var arrayTwo = [26, 134, 135, 15, 17]
-print(moveElementToEnd(array: &arrayOne, 2))
+print(isMonotonic(array: [-1, -2, -3, -4]))
