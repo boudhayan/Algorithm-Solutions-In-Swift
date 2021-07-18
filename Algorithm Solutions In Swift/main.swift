c4da871a02dc9dc517fcc6ffa43efe5fd810c9ea
Boudhayan Biswas <hello@boudhayan.com>
2021-07-18 09:46:29 +0530

    Add smallest difference

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 print("Hello, World!")
-
-print(firstNonRepeatingCharacter("faadabcbbebdf"))
+var arrayOne = [-1, 5, 10, 20, 28, 3]
+var arrayTwo = [26, 134, 135, 15, 17]
+print(smallestDifference(arrayOne: &arrayOne, arrayTwo: &arrayTwo))
