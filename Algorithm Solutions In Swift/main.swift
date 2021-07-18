633183f2c8077778016cf4bfc4a40d0b868fd021
Boudhayan Biswas <hello@boudhayan.com>
2021-07-18 10:45:53 +0530

    Add array of products

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -10,4 +10,4 @@ import Foundation
 print("Hello, World!")
 var arrayOne = [2, 1, 2, 2, 2, 3, 4, 2]
 var arrayTwo = [26, 134, 135, 15, 17]
-print(isMonotonic(array: [-1, -2, -3, -4]))
+print(arrayOfProducts([5, 1, 4, 2]))
