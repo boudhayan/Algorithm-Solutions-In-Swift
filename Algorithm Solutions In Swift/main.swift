4aa980dc8823fbf6d843129499dfa958f178aafc
Boudhayan Biswas <hello@boudhayan.com>
2021-07-23 14:40:59 +0530

    Add spiral traversal of 2d matrix

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -18,5 +18,24 @@ root.right?.left = BSTNode(value: 4)
 root.right?.right = BSTNode(value: 6)
 root.right?.right?.right = BSTNode(value: 7)
 
-var array = [1, 1, 1, 1, 2]
-print(hasSingleCycle(array: array))
+var matrix1 = [
+    [1,   2,  3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10,  9,  8, 7]
+]
+
+var matrix2 = [
+    [1, 2],
+    [6, 3],
+    [5, 4]
+]
+
+var matrix3 = [
+    [1],
+    [2],
+    [3],
+    [4]
+]
+print(spiralTraverse(array: matrix3))
+
