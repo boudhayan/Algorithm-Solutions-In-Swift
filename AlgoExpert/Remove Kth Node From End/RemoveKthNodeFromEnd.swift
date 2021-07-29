623cb754d9f29bfaa0106c60d06a23c1d44abab3
Boudhayan Biswas <hello@boudhayan.com>
2021-07-30 01:14:33 +0530

    Add remove Kth node from end using two pointer approach

--- AlgoExpert/Remove Kth Node From End/RemoveKthNodeFromEnd.swift
+++ AlgoExpert/Remove Kth Node From End/RemoveKthNodeFromEnd.swift
@@ -10,7 +10,7 @@ import Foundation
 /**
  Time Complexity: O(n)
  Space Complexity: O(1)
- Note: 
+ Note:
  */
 func removeKthNodeFromEnd(head: LinkedList, k: Int) {
     let positionToBeDeleted = getLength(from: head) - k
@@ -39,4 +39,34 @@ func getLength(from head: LinkedList) -> Int {
         currentNode = currentNode?.next
     }
     return length
+}
+
+/**
+ Time Complexity: O(n)
+ Space Complexity: O(1)
+ Note: 
+ */
+func removeKthNodeFromEndTwoPointer(head: LinkedList, k: Int) {
+    var firstPointer: LinkedList? = head
+    var secondPointer: LinkedList? = head
+    var counter = 1
+    while counter <= k {
+        counter += 1
+        secondPointer = secondPointer?.next
+    }
+    
+    if secondPointer == nil {
+        // node to be deleted is the head
+        // change the value of the head and next pointer
+        head.value = firstPointer!.next!.value
+        head.next = firstPointer?.next?.next
+        return
+    }
+    
+    // visit till (k - 1)th node, so that first pointer points to the previous node of node to be deleted
+    while secondPointer?.next != nil {
+        firstPointer = firstPointer?.next
+        secondPointer = secondPointer?.next
+    }
+    firstPointer?.next = firstPointer?.next?.next
 }
