1af41b29fad678fb189a39f2d1d1c9ceecacc633
Boudhayan Biswas <hello@boudhayan.com>
2021-09-24 00:35:51 +0530

    Add Single Number

---
+++ LeetCode/Single Number/SingleNumber.swift
@@ -0,0 +1,16 @@
+//
+//  SingleNumber.swift
+//  Algorithm Solutions In Swift
+//
+//  Created by Boudhayan Biswas on 24/09/21.
+//
+
+import Foundation
+
+func singleNumber(_ nums: [Int]) -> Int {
+      var num = nums[0]
+      for idx in 1..<nums.count {
+          num = num ^ nums[idx]
+      }
+      return num
+  }
