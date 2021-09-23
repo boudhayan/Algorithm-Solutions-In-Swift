4be3138b74c73abec5814aebe259b6554806d4c0
Boudhayan Biswas <hello@boudhayan.com>
2021-09-24 01:10:26 +0530

    Add binary search

--- LeetCode/Binary Search/BinarySearchL.swift
+++ LeetCode/Binary Search/BinarySearchL.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+func search(_ nums: [Int], _ target: Int) -> Int {
+      var low = 0
+      var high = nums.count - 1
+      while low <= high {
+          let mid = low + (high - low) / 2
+          if target < nums[mid] {
+              high = mid - 1
+          } else if target > nums[mid] {
+              low = mid + 1
+          } else {
+              return mid
+          }
+      }
+      return -1
+  }
