92c7ff2c6916cbaff9b49d11f5a845d0e4e5f354
Boudhayan Biswas <hello@boudhayan.com>
2021-09-24 01:11:36 +0530

    Add First Bad version

--- LeetCode/First Bad Version/FirstBadVersion.swift
+++ LeetCode/First Bad Version/FirstBadVersion.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+func firstBadVersion(_ n: Int) -> Int {
+    var low = 1
+    var high = n
+    var badVersion = n
+    while low <= high {
+        let mid = low + (high - low) / 2
+        if isBadVersion(mid) {
+            high = mid - 1
+            badVersion = min(badVersion, mid)
+        } else {
+            low = mid + 1
+        }
+    }
+    return badVersion
+}
+
+func isBadVersion(_ version: Int) -> Bool {
+    //implementation needed
+}
