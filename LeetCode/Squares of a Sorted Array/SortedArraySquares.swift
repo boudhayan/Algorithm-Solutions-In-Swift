c3559687282eb82896d2ef8f1e8384a91391838b
Boudhayan Biswas <hello@boudhayan.com>
2021-09-25 00:55:20 +0530

    Add sorted squares of an array

--- LeetCode/Squares of a Sorted Array/SortedArraySquares.swift
+++ LeetCode/Squares of a Sorted Array/SortedArraySquares.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+func sortedSquares(_ nums: [Int]) -> [Int] {
+    guard nums.count > 0 else { return [] }
+    var result = Array(repeating: 0, count: nums.count)
+    var low = 0
+    var high = nums.count - 1
+    var position = nums.count - 1
+    while low <= high, position >= 0 {
+        if abs(nums[low]) > abs(nums[high]) {
+            // insert low
+            result[position] = nums[low] * nums[low]
+            low += 1
+        } else {
+            // insert high
+            result[position] = nums[high] * nums[high]
+            high -= 1
+        }
+        position -= 1
+    }
+    return result
+}
