6ea2ed7932e902fb54da492be3ef89428c6c2ef5
Boudhayan Biswas <hello@boudhayan.com>
2021-09-25 00:56:06 +0530

    Add rotate array

--- LeetCode/Rotate Array/RotateArray.swift
+++ LeetCode/Rotate Array/RotateArray.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func rotate(_ nums: inout [Int], _ k: Int) {
+    guard nums.count > 1 else { return }
+    let rc = k % nums.count
+    reverse(start: 0, end: nums.count - 1, array: &nums)
+    reverse(start: 0, end: rc - 1, array: &nums)
+    reverse(start: rc, end: nums.count - 1, array: &nums)
+}
+    
+func reverse(start: Int, end: Int, array: inout [Int]) {
+    var s = start
+    var e = end
+    while s < e {
+        array.swapAt(s, e)
+        s += 1
+        e -= 1
+    }
+}
