7102ada14fc6583434a543abf8da0aa1d5a53c9a
Boudhayan Biswas <hello@boudhayan.com>
2022-03-10 23:09:54 +0530

    Add merge sort

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -7,50 +7,8 @@
 
 import Foundation
 
-var arrayOne = [2, 1, 2, 2, 2, 3, 4, 2]
-var arrayTwo = [26, 134, 135, 15, 17]
+var array = [26, 134, 135, 15, 17]
 
-var root = BSTNode(value: 3)
-root.left = BSTNode(value: 2)
-root.left?.left = BSTNode(value: 1)
-root.right = BSTNode(value: 5)
-root.right?.left = BSTNode(value: 4)
-root.right?.right = BSTNode(value: 6)
-root.right?.right?.right = BSTNode(value: 7)
+print(mergeSort(array))
 
-var matrix1 = [
-    [1,   2,  3, 4],
-    [12, 13, 14, 5],
-    [11, 16, 15, 6],
-    [10,  9,  8, 7]
-]
-
-var matrix2 = [
-    [1, 2],
-    [6, 3],
-    [5, 4]
-]
-
-var matrix3 = [
-    [1],
-    [2],
-    [3],
-    [4]
-]
-
-
-
-var bt = BinaryTree(value: 1)
-bt.left = BinaryTree(value: 2)
-bt.left?.left = BinaryTree(value: 4)
-bt.left?.right = BinaryTree(value: 5)
-bt.left?.left?.left = BinaryTree(value: 6)
-bt.right = BinaryTree(value: 3)
-
-var arrayToTraverse = [
-    [1, 3,  4,  10],
-    [2, 5,  9,  11],
-    [6, 8,  12, 15],
-    [7, 13, 14, 16]
-]
-//[0, 1, 21, 33, 45, 45, 45, 45, 45, 45, 61, 72, 73]
+print(array[0...0])
