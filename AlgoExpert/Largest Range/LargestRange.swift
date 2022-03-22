c8ed8dca143adb54cbf8affe09f580d8daac288c
Boudhayan Biswas <hello@boudhayan.com>
2022-03-22 10:49:30 +0530

    Add largest range using hashmap

--- AlgoExpert/Largest Range/LargestRange.swift
+++ AlgoExpert/Largest Range/LargestRange.swift
@@ -8,49 +8,46 @@
 import Foundation
 
 /**
- Time Complexity: O(nlogn)
- Space Complexity: O(1)
+ Time Complexity: O(n)
+ Space Complexity: O(n)
  Note: 
  */
 func largestRange(array: [Int]) -> [Int] {
-    let sortedNumbers = array.sorted()
-    var rangeFound = false
-    var start = -1
-    var end = -1
-    var rangeLength = 0
-    var maxLength = -1
-    var result = [Int]()
-    guard array.count > 1 else { return [array[0], array[0]]}
-    for idx in 1..<sortedNumbers.count {
-        let diff = sortedNumbers[idx] - sortedNumbers[idx - 1]
-        if diff == 1 {
-            if rangeFound == false {
-                rangeFound = true
-                start = sortedNumbers[idx - 1]
-                rangeLength += 1
-            } else {
-                rangeLength += 1
-                end = sortedNumbers[idx]
+    var dict = [Int: Bool]()
+    for num in array {
+        dict[num] = false
+    }
+    var range = [Int]()
+    var length = 0
+    
+    for num in array {
+        if dict[num] != nil, dict[num]! == false {
+            // explore range
+            // explore left
+            var localMin = num
+            var localMax = num
+            var localLength = 1
+            dict[num] = true
+            var n = num
+            while dict[n - 1] != nil {
+                dict[n - 1] = true
+                localMin = n - 1
+                localLength += 1
+                n -= 1
             }
-           
-        }
-        
-        if diff != 1, diff != 0 {
-            if rangeFound {
-                end = sortedNumbers[idx - 1]
-                if rangeLength > maxLength {
-                    maxLength = rangeLength
-                    result = [start, end]
-                    rangeLength = 0
-                    start = -1
-                    end = -1
-                }
+            
+            n  = num
+            while dict[n + 1] != nil {
+                dict[n + 1] = true
+                localMax = n + 1
+                localLength += 1
+                n += 1
             }
-            rangeFound = false
+            if localLength >= length {
+                length = localLength
+                range = [localMin, localMax]
+            }
         }
     }
-    if rangeFound, rangeLength > maxLength {
-        result = [start, sortedNumbers[sortedNumbers.count - 1]]
-    }
-    return result
+    return range
 }
--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -9,8 +9,46 @@ import Foundation
 import CloudKit
 
 var reds = [5, 5, 3, 9, 2]
-var blues = [3, 6, 7, 2, 1]
+var array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
 
+func largestRange1(array: [Int]) -> [Int] {
+    var dict = [Int: Bool]()
+    for num in array {
+        dict[num] = false
+    }
+    var range = [Int]()
+    var length = 0
+    
+    for num in array {
+        if dict[num] != nil, dict[num]! == false {
+            // explore range
+            // explore left
+            var localMin = num
+            var localMax = num
+            var localLength = 1
+            dict[num] = true
+            var n = num
+            while dict[n - 1] != nil {
+                dict[n - 1] = true
+                localMin = n - 1
+                localLength += 1
+                n -= 1
+            }
+            
+            n  = num
+            while dict[n + 1] != nil {
+                dict[n + 1] = true
+                localMax = n + 1
+                localLength += 1
+                n += 1
+            }
+            if localLength >= length {
+                length = localLength
+                range = [localMin, localMax]
+            }
+        }
+    }
+    return range
+}
 
-
-print(subarraySort1(array: [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]))
+print(largestRange1(array: array))
