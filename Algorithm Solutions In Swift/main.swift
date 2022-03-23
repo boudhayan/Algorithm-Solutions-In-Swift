e6876b3840997da71af47e0ec3b7520dc0a385df
Boudhayan Biswas <hello@boudhayan.com>
2022-03-24 00:24:21 +0530

    Add smallest substring of all characters

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -11,11 +11,8 @@ import CloudKit
 var reds = [5, 5, 3, 9, 2]
 var array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
 
+var colors = [0, 1, 2]
 
 
-print(minRewards1([8, 4, 2, 1, 3, 6, 7, 9, 5]))
-// 1
-
-// [8, 4, 2, 1, 3, 6, 7, 9, 5]
-// [1, 1, 1, 1, 1, 1, 1, 1, 1]
-// [1, 1, 1, 1, 2, 3, 4, 5, 1]
+print(getShortestUniqueSubstring(arr: ["A","B","C","E","K","I"], str: "KADOBECODEBANCDDDEI"))
+//xyyzyzyx
