00e02ded3e2460e63c310eb5831e704ed417229b
Boudhayan Biswas <hello@boudhayan.com>
2022-03-25 00:13:14 +0530

    Add Kadanes Algorithm

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -11,8 +11,16 @@ import CloudKit
 var reds = [5, 5, 3, 9, 2]
 var array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
 
-var colors = [0, 1, 2]
+var colors = [-2,1,-3,4,-1,2,1,-5,4]
 
+func maxSubArray(_ nums: [Int]) -> Int {
+      var maxSum = Int(Int32.min)
+       var currentSum = 0
+       for num in nums {
+           currentSum = max(0, currentSum + num)
+           maxSum = max(currentSum, maxSum)
+       }
+       return maxSum
+   }
 
-print(getShortestUniqueSubstring(arr: ["A","B","C","E","K","I"], str: "KADOBECODEBANCDDDEI"))
-//xyyzyzyx
+print(maxSubArray(colors))
