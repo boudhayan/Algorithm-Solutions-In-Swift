8648540a6cacf3c5a07eb6a76bb3fe138af12ab8
Boudhayan Biswas <hello@boudhayan.com>
2022-03-25 14:59:57 +0530

    Add apartment hunting

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -12,15 +12,38 @@ var reds = [5, 5, 3, 9, 2]
 var array = [1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6]
 
 var colors = [-2,1,-3,4,-1,2,1,-5,4]
+let blocks = [
+    [
+        "gym": false,
+        "school": true,
+        "store": false
+    ],
+    [
+        "gym": true,
+        "school": false,
+        "store": false
+    ],
+    [
+        "gym": true,
+        "school": true,
+        "store": false
+    ],
+    [
+        "gym": false,
+        "school": true,
+        "store": false
+    ],
+    [
+        "gym": false,
+        "school": true,
+        "store": true
+    ]
+]
 
-func maxSubArray(_ nums: [Int]) -> Int {
-      var maxSum = Int(Int32.min)
-       var currentSum = 0
-       for num in nums {
-           currentSum = max(0, currentSum + num)
-           maxSum = max(currentSum, maxSum)
-       }
-       return maxSum
-   }
+let reqs = ["gym", "school", "store"]
 
-print(maxSubArray(colors))
+
+
+
+
+print(apartmentHunting1(blocks, reqs))
