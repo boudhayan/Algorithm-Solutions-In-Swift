7ab4c130e4cfa27529c2dbeff7af9a66d0624750
Boudhayan Biswas <hello@boudhayan.com>
2022-04-26 10:11:44 +0530

    Add best time to buy sell stock using two pointer

--- LeetCode/Best Time to Buy and Sell Stock/BuySellStock.swift
+++ LeetCode/Best Time to Buy and Sell Stock/BuySellStock.swift
@@ -17,3 +17,20 @@ func maxProfit(_ prices: [Int]) -> Int {
     }
     return profit
 }
+
+// using two pointer
+func maxProfit2(_ prices: [Int]) -> Int {
+    var l = 0
+    var r = 1
+    var maxP = 0
+    while r < prices.count {
+        // proftiable transaction
+        if prices[l] < prices[r] {
+            maxP = max(maxP, prices[r] - prices[l])
+        } else {
+            l = r
+        }
+        r += 1
+    }
+    return maxP
+}
