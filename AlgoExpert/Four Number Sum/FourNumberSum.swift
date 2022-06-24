18f0d5319fd78c596f5a1290717baccde8295b54
Boudhayan Biswas <hello@boudhayan.com>
2022-06-24 16:07:20 +0530

    Update FourNumberSum.swift

--- AlgoExpert/Four Number Sum/FourNumberSum.swift
+++ AlgoExpert/Four Number Sum/FourNumberSum.swift
@@ -8,35 +8,48 @@
 import Foundation
 
 /**
- Time Complexity: O(n^3)
+ Time Complexity: O(n^2)
  Space Complexity: O(n^2)
- Note: 
+ Note:
+ 1. Time complexity is O(n^2) as there are two for loop, for each iteration of outer for loop, inner loops iterates n times.
+ 2: Space complexity is O(n^2) as at most you will store n * n pairs in dictionary.
+  for 1 element, pairs count is 1
+for 2 element, pairs count is 2
+ for 3 element, pairs count is 3
+ for n elements, pairs count is n
+ total count is = 1 + 2+ 3 + ...+n => n*(n + 1) / 2 => O(n^2)
  */
 func fourNumberSum(array: [Int], targetSum: Int) -> [[Int]] {
-    guard array.count > 3 else { return [] }
-    var result = [[Int]]()
-    let sortedNumbers = array.sorted()
-    for idx in 0..<sortedNumbers.count - 3 {
-        let key = sortedNumbers[idx]
-        for j in idx+1..<sortedNumbers.count - 2 {
-            let secondNumber = sortedNumbers[j]
-            var leftIdx = j + 1
-            var rightIdx = sortedNumbers.count - 1
-            while leftIdx < rightIdx {
-                let currentSum = key + secondNumber + sortedNumbers[leftIdx] + sortedNumbers[rightIdx]
-                if currentSum < targetSum {
-                    leftIdx += 1
-                } else if currentSum > targetSum {
-                    rightIdx -= 1
-                } else {
-                    result.append([key, secondNumber, sortedNumbers[leftIdx], sortedNumbers[rightIdx]])
-                    leftIdx += 1
-                    rightIdx -= 1
+    var results = [[Int]]()
+    // dictionary to store the pairs
+    var pairs = [Int: [[Int]]]()
+    // start from 1 through array.count - 1, as for the last iteration we should have two numbers, other's two if found will get from dictionary of pairs
+    for i in 1..<array.count - 1 {
+        // key number
+        let key = array[i]
+        for j in i+1..<array.count {
+            // we will go thorugh from key's next position to last, to check every pairs sum
+            let cs = key + array[j]
+            // check remaianing sum
+            let remaining = targetSum - cs
+            // check if there is any existing pairs in dictionary with remaining sum as their total
+            if let existing = pairs[remaining] {
+                for pair in existing {
+                    results.append([key, array[j]] + pair)
                 }
             }
         }
-        
-        
+        // generate all the new pairs with key as fixed number and all the of previous numbers of key
+        var k = 0
+        while k < i {
+            let sum = array[k] + array[i]
+            if let others = pairs[sum] {
+                pairs[sum] = others + [[array[k], array[i]]]
+            } else {
+                pairs[sum] = [[array[k], array[i]]]
+            }
+            k += 1
+        }
     }
-    return result
+    return results
 }
