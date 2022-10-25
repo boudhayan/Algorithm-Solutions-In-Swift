62ecec246276603060fd959b1d617355e6b53a14
Boudhayan Biswas <hello@boudhayan.com>
2022-10-25 08:15:55 +0530

    Add rotate image

--- LeetCode/Rotate Image/RotateImage.swift
+++ LeetCode/Rotate Image/RotateImage.swift
@@ -20,3 +20,48 @@ func rotate(_ matrix: inout [[Int]]) {
         }
     }
 }
+
+func rotateImage1(_ matrix: inout [[Int]]) {
+         var firstRow = 0
+   var firstColumn = 0
+   var lastRow = matrix.count - 1
+   var lastColumn = matrix[0].count - 1
+   let c = matrix.count - 1
+   while firstRow < lastRow, firstColumn < lastColumn {
+       // push first row values in last column
+       var p = firstRow
+       while p < lastRow {
+           let temp = matrix[firstRow][p]
+           matrix[firstRow][p] = matrix[p][lastColumn]
+           matrix[p][lastColumn] = temp
+           p += 1
+       }
+       
+       // interchange first temp row with last row
+       p = firstColumn
+       var col = lastColumn
+       while col >= firstColumn {
+           let temp = matrix[lastRow][col]
+           matrix[lastRow][col] = matrix[firstRow][p]
+           matrix[firstRow][p] = temp
+           col -= 1
+           p += 1
+       }
+       
+       // interchange first temp row with first column
+       p = firstColumn
+       var row = lastRow
+       while row >= firstRow {
+           let temp = matrix[row][firstColumn]
+           matrix[row][firstColumn] = matrix[firstRow][p]
+           matrix[firstRow][p] = temp
+           row -= 1
+           p += 1
+       }
+       firstRow += 1
+       firstColumn += 1
+       lastRow -= 1
+       lastColumn -= 1
+
+   }
+   }
