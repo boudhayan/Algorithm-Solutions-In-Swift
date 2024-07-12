054928536a40ca23aa9c6c7507602a131e82ca5c
Boudhayan Biswas <mail2boudhayan@gmail.com>
2024-07-12 22:23:27 +0530

    add maximum gain

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -356,3 +356,337 @@ func reversePrefix(_ word: String, _ ch: Character) -> String {
 }
 
 print(reversePrefix("abcdefd", "d"))
+
+private class ListNode {
+    var val: Int
+    var next: ListNode?
+    
+    init(val: Int, next: ListNode? = nil) {
+        self.val = val
+        self.next = next
+    }
+}
+
+private func doubleIt(_ head: ListNode?) -> ListNode? {
+    var current = head
+    var prev: ListNode? = nil
+    while current != nil {
+        let temp = current?.next
+        current?.next = prev
+        prev = current
+        current = temp
+    }
+    
+    current = prev
+    var revHead = current
+    var carry = 0
+    while current != nil {
+        let newVal = current!.val * 2 + carry
+        carry = newVal/10
+        current?.val = newVal%10
+        prev = current
+        current = current?.next
+    }
+    if carry > 0 {
+        prev?.next = ListNode(val: carry)
+    }
+    current = revHead
+    prev = nil
+    while current != nil {
+        let temp = current?.next
+        current?.next = prev
+        prev = current
+        current = temp
+    }
+    return prev
+}
+
+
+private var ll = ListNode(val: 9)
+ll.next = ListNode(val: 9)
+ll.next?.next = ListNode(val: 9)
+
+private func trav(_ head: ListNode?) {
+    var current = head
+    while let node = current {
+        print(node.val)
+        current = current?.next
+    }
+}
+
+trav(doubleIt(ll))
+
+func getMaximumGold(_ grid: [[Int]]) -> Int {
+    var res = 0
+    let ROWS = grid.count, COLS = grid[0].count
+    func dfs(r: Int, c: Int, visited: inout Set<[Int]>) -> Int {
+        if r < 0 || c < 0 || r == ROWS || c == COLS || grid[r][c] == 0 || visited.contains([r, c]) {
+            return 0
+        }
+        visited.insert([r, c])
+        var res = grid[r][c]
+        let neighbours = [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]
+        for neighbour in neighbours {
+            res = max(res, grid[r][c] + dfs(r: neighbour[0], c: neighbour[1], visited: &visited))
+        }
+        visited.remove([r, c])
+        return res
+    }
+    
+    for row in 0..<grid.count {
+        for col in 0..<grid[0].count {
+            var visited = Set<[Int]>()
+            res = max(res, dfs(r: row, c: col, visited: &visited))
+        }
+    }
+    return res
+}
+
+
+print(getMaximumGold([[0,6,0],[5,8,7],[0,9,0]]))
+
+
+private func evaluateTree(_ root: TreeNode?) -> Bool {
+    
+    func helper(_ node: TreeNode?) -> Bool {
+        if node?.left == nil, node?.right == nil {
+            return node!.val == 1 ? true : false
+        }
+        if node!.val == 2 {
+            return helper(node?.left) || helper(node?.right)
+        } else {
+            return helper(node?.left) && helper(node?.right)
+        }
+    }
+    
+    return helper(root)
+}
+
+private let b = TreeNode(val: 1)
+b.left = TreeNode(val: 2)
+b.left?.left = TreeNode(val: 2)
+//b.right = TreeNode(val: 3)
+//b.right?.left = TreeNode(val: 0)
+//b.right?.right = TreeNode(val: 1)
+
+
+
+private func subsets1(_ nums: [Int]) -> [[Int]] {
+    var res = [[Int]]()
+    var curr = [Int]()
+    func helper(first: Int = 0, curr: inout [Int], size: Int) {
+        if size == curr.count {
+            res.append(curr)
+            return
+        }
+        for i in first..<nums.count {
+            curr.append(nums[i])
+            helper(first: i + 1, curr: &curr, size: size)
+            curr.removeLast()
+        }
+    }
+    
+    for s in 0...nums.count {
+        helper(curr: &curr, size: s)
+    }
+    return res
+}
+
+print(subsets1([1, 2, 3]))
+
+
+func specialArray(_ nums: [Int]) -> Int {
+    let arr = nums.sorted()
+    var guess = nums.count
+    while guess > 0 {
+        var l = 0
+        var r = arr.count - 1
+        while r < arr.count, l < r {
+            let m = (l + r) / 2
+            if arr[m] >= guess {
+                r = m
+            } else {
+                l = m + 1
+            }
+        }
+        if arr[l] >= guess, nums.count - r == guess {
+            break
+        }
+        guess -= 1
+    }
+    return guess == 0 ? -1 : guess
+}
+
+
+print(specialArray([3,9,7,8,3,8,6,6]))
+
+func replaceWords(_ dictionary: [String], _ sentence: String) -> String {
+    var words = [String]()
+    for word in sentence.components(separatedBy: " ") {
+        var newWord = word
+        for dictWord in dictionary where word.hasPrefix(dictWord) && dictWord.count < newWord.count {
+            newWord = dictWord
+        }
+            words.append(newWord)
+        }
+    return words.joined(separator: " ")
+}
+
+
+print(replaceWords(["a", "aa", "aaa", "aaaa"], "a aa a aaaa aaa aaa aaa aaaaaa bbb baba ababa"))
+
+
+
+func judgeSquareSum(_ c: Int) -> Bool {
+    var high = Int(ceil(sqrt(Double(c))))
+    var low = 0
+    while low <= high {
+        let num = low * low + high * high
+        if num < c {
+            low += 1
+        } else if num > c {
+            high -= 1
+        } else {
+            return true
+        }
+    }
+    return false
+}
+
+
+print(judgeSquareSum(4))
+
+
+func maxProfitAssignment(_ difficulty: [Int], _ profit: [Int], _ worker: [Int]) -> Int {
+    var netProfit = 0
+    let arr = zip(difficulty, profit).sorted { $0.0 > $1.0 }
+    for work in worker {
+        var low = 0
+        var high = arr.count - 1
+        var jobProfit = 0
+        while low <= high {
+            let mid = (low + high) / 2
+            if arr[mid].0 <= work {
+                jobProfit = max(jobProfit, arr[mid].1)
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+        netProfit += jobProfit
+    }
+    return netProfit
+}
+
+print(maxProfitAssignment([2,4,6,8,10], [10,20,30,40,50], [4,5,6,7]))
+
+func numberOfSubarrays(_ nums: [Int], _ k: Int) -> Int {
+    var res = 0
+    var odd = 0
+    var l = 0, m = 0
+    for r in 0..<nums.count {
+        if nums[r] % 2 == 1 {
+            odd += 1
+        }
+        while odd > k {
+            if nums[l] % 2 == 1 {
+                odd -= 1
+            }
+            l += 1
+            m = l
+        }
+        
+        if odd == k {
+            while nums[m] % 2 != 1 {
+                m += 1
+            }
+            res += m - l + 1
+        }
+    }
+    return res
+}
+
+
+func minDifference(_ nums: [Int]) -> Int {
+    var arr = nums.sorted()
+    var c = 0
+    while c < 3 {
+        arr[arr.count - 1 - c] = arr[0]
+        c += 1
+    }
+    return arr[arr.count - 1 - 3] - arr[0]
+}
+
+print(minDifference([6,6,0,1,1,4,6]))
+//0, 1, 1, 4, 6, 6, 6
+
+private func nodesBetweenCriticalPoints(_ head: ListNode?) -> [Int] {
+    var previous: ListNode? = head
+    var current: ListNode? = head?.next
+    var dist = 1
+    var criticalCount = 0
+    var first = 0
+    var last = 0
+    var minDistance = Int(Int32.max)
+    while let previosNode = previous, let currentNode = current, let nextNode = current?.next {
+        if (currentNode.val > previosNode.val && currentNode.val > nextNode.val) || (currentNode.val < previosNode.val && currentNode.val < nextNode.val) {
+            // found local maxima or minima
+            if first == 0 {
+                first = dist
+            } else if first > 0 || last > 0 {
+                minDistance = min(minDistance, dist - max(first, last))
+                last = dist
+            }
+            
+            criticalCount += 1
+        }
+        dist += 1
+        previous = currentNode
+        current = currentNode.next
+    }
+    return criticalCount < 2 ? [-1, -1] : [minDistance, last - first]
+}
+
+private let ll1 = ListNode(val: 1)
+ll1.next = ListNode(val: 3)
+ll1.next?.next = ListNode(val: 2)
+ll1.next?.next?.next = ListNode(val: 2)
+ll1.next?.next?.next?.next = ListNode(val: 3)
+ll1.next?.next?.next?.next?.next = ListNode(val: 2)
+
+print(nodesBetweenCriticalPoints(ll1))
+
+func numWaterBottles(_ numBottles: Int, _ numExchange: Int) -> Int {
+    var res = numBottles
+    var totalBottles = numBottles
+    while totalBottles >= numExchange {
+        let drunk = totalBottles / numExchange
+        res += drunk
+        totalBottles = (totalBottles % numExchange) + (totalBottles / numExchange)
+    }
+    return res
+}
+
+print(numWaterBottles(15, 4))
+
+func averageWaitingTime(_ customers: [[Int]]) -> Double {
+    var currentTime = 0
+    var totalWaitTime = 0
+    for i in 0..<customers.count {
+        let customer = customers[i]
+        let actualStartTime = customer[0], duration = customer[1]
+        var waitTime = 0
+        if currentTime < actualStartTime {
+            waitTime = duration
+            currentTime = actualStartTime + duration
+        } else {
+            waitTime = currentTime + duration - actualStartTime
+            currentTime = currentTime + duration
+        }
+        totalWaitTime += waitTime
+    }
+    return Double(totalWaitTime) / Double(customers.count)
+}
+
+
+print(averageWaitingTime([[5,2],[5,4],[10,3],[20,1]]))
