d9b5b8c27241a24b41ff2435c79b6a3fa988a0b1
Boudhayan <mail2boudhayan@gmail.com>
2026-02-26 16:44:32 +0530

    add array sorted and rotated

--- Algorithm Solutions In Swift/main.swift
+++ Algorithm Solutions In Swift/main.swift
@@ -1,21 +1,7 @@
 import Foundation
 
 
-//func insertionSort(array: inout [Int]) -> [Int] {
-//    for idx in 1..<array.count {
-//        var sortedIdx = idx - 1
-//        let key = array[idx]
-//        while sortedIdx >= 0, key < array[sortedIdx] {
-//            array[sortedIdx + 1] = array[sortedIdx]
-//            sortedIdx -= 1
-//        }
-//        array[sortedIdx + 1] = key
-//    }
-//    return array
-//}
 
 
+var array = [3, 4, 5, 1, 2]
 
-var array = [5, 3, 1, 7, 2]
-recursiveInsertionSort(&array)
-print(array)
